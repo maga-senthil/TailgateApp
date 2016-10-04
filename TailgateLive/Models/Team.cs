41ca4b252c08814635a1d4068562eb454a9c815a
maga-senthil <magalakshmi.s@gmail.com>
2016-10-04 13:39:34 -0500

    initial commit

---
+++ TailgateLive/Models/Team.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TailgateLive.Models
+{
+    public class Team
+    {
+        [Key]
+        public int Id { get; set; }
+        public string TeamName { get; set; }
+        public ICollection <User> Users { get; set; }
+    }
+}
---
+++ TailgateLive/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TailgateLive.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public int UserZipCode { get; set; }
+        public int UserRating { get; set; }
+        [ForeignKey ("ApplicationUsers")]
+        public string EmailId { get; set; }
+        public ApplicationUser ApplicationUsers { get; set; }
+        public ICollection <Event> Events { get;set; }
+        public ICollection <Team> Teams { get; set; }
+
+
+    }
+}
