d5dfb1d819bcc9fb4a97af153253bf77649534ad
maga-senthil <magalakshmi.s@gmail.com>
2016-10-04 15:24:32 -0500

    testing

--- TailgateLive/Migrations/201610042023304_newmigration1.Designer.cs
+++ TailgateLive/Migrations/201610042023304_newmigration1.Designer.cs
@@ -7,13 +7,13 @@ namespace TailgateLive.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class test : IMigrationMetadata
+    public sealed partial class newmigration1 : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(test));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(newmigration1));
         
         string IMigrationMetadata.Id
         {
-            get { return "201610041937353_test"; }
+            get { return "201610042023304_newmigration1"; }
         }
         
         string IMigrationMetadata.Source
