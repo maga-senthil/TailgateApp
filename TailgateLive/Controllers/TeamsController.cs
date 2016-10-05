64e381cafdb142c34cd8f7c6653a060aac10baa1
erikthomasjohnson <erikjohnsoncomposer@gmail.com>
2016-10-05 08:03:40 -0500

    Added async GET

--- TailgateLive/Controllers/TeamsController.cs
+++ TailgateLive/Controllers/TeamsController.cs
@@ -4,6 +4,9 @@ using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using TailgateLive.Models;
@@ -122,6 +125,60 @@ namespace TailgateLive.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        public class Product
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+            public decimal Price { get; set; }
+            public string Category { get; set; }
+        }
+
+        class Program
+        {
+            static HttpClient client = new HttpClient();
+
+            static void ShowProduct(Product product)
+            {
+                Console.WriteLine($"Name: {product.Name}\tPrice: {product.Price}\tCategory: {product.Category}");
+            }
+
+            static async Task<Product> GetProductAsync(string path)
+            {
+                Product product = null;
+                HttpResponseMessage response = await client.GetAsync(path);
+                if (response.IsSuccessStatusCode)
+                {
+                    product = await response.Content.ReadAsAsync<Product>();
+                }
+                return product;
+            }
+
+            static async Task RunAsync()
+            {
+                client.BaseAddress = new Uri("http://localhost:55268/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                try
+                {
+                    // Create a new product
+                    Product product = new Product { Name = "Gizmo", Price = 100, Category = "Widgets" };
+
+                    // Get the product
+                    string path = "http://www.fantasyfootballnerd.com/service/schedule/json/whmbtmf4wbqd/";
+                    product = await GetProductAsync(path);
+                    ShowProduct(product);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                Console.ReadLine();
+            }
+
         }
     }
 }
