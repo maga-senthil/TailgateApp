7219a63bb1d02e9f1c84ea3cf8613b0b62b4ef1a
erikthomasjohnson <erikjohnsoncomposer@gmail.com>
2016-10-07 08:53:04 -0500

    Added Teams

--- TailgateLive/Controllers/TeamsController.cs
+++ TailgateLive/Controllers/TeamsController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using TailgateLive.Models;
+using System.Threading.Tasks;
 
 namespace TailgateLive.Controllers
 {
@@ -46,16 +47,46 @@ namespace TailgateLive.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        //public ActionResult CreateOld([Bind(Include = "Id,TeamName")] Team team)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.TeamDb.Add(team);
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    return View(team);
+        //}
+
         public ActionResult Create([Bind(Include = "Id,TeamName")] Team team)
         {
+            Team tempTeam = new Team();
+            foreach (string t in GetTeamNames())
+            {
+                tempTeam.TeamName = t;
+                db.TeamDb.Add(tempTeam);
+                db.SaveChanges();
+            }
             if (ModelState.IsValid)
             {
-                db.TeamDb.Add(team);
-                db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
             return View(team);
+        }
+        public List<string> GetTeamNames()
+        {
+            List<List<string>> teamsList = new List<List<string>>();
+            teamsList = NFL_API.GET_NFL.RunAsyncNFLTeam();
+            List<string> teamNameLong = new List<string>();
+            string name;
+            for (int i = 0; i < teamsList.Count; i++)
+            {
+                name = teamsList[i][1];
+                teamNameLong.Add(name);
+            }
+            return teamNameLong;
         }
 
         // GET: Teams/Edit/5
---
+++ TailgateLive/Models/GameSchedule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TailgateLive.Models
+{
+    public class GameSchedule
+    {
+        [Key]
+        public int Id { get; set; }
+
+    }
+}
