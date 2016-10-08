bffc18961d32485cd4c79667067e309bad72a0d4
marinaher <b-her@live.com>
2016-10-08 13:12:27 -0500

    updates on css

--- TailgateLive/Scripts/bootstrap.js
+++ TailgateLive/Scripts/bootstrap.js
@@ -327,7 +327,7 @@ if (!jQuery) { throw new Error("Bootstrap requires jQuery") }
   }
 
   Carousel.DEFAULTS = {
-    interval: 5000
+      interval: 999995000
   , pause: 'hover'
   , wrap: true
   }
